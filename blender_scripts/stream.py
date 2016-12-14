07a7237b6f647ed3c91773d644273f20deae6e45
Mathias@banana <m.sunardi@gmail.com>
2016-12-14 10:27:11 -0800

    Add joints

--- blender_scripts/stream.py
+++ blender_scripts/stream.py
@@ -6,9 +6,13 @@ r = redis.StrictRedis(host="localhost", port=6379, db=0)
 Armature = bpy.data.objects["Female1_D3_ConversationGestures"]
 Arm = Armature.data
 
-Neck = Arm.bones["Neck"]
+eck = Arm.bones["Neck"]
 PoseNeck = Armature.pose.bones["Neck"]
 PoseHead = Armature.pose.bones["Head"]
+PoseRElbow = Armature.pose.bones["RightForeArm"]
+PoseLElbow = Armature.pose.bones["LeftForeArm"]
+PoseRShoulder = Armature.pose.bones["RightArm"]
+PoseLShoulder = Armature.pose.bones["LeftArm"]
 
 def fhandler(scene):
     i = bpy.context.scene.frame_current
@@ -17,9 +21,17 @@ def fhandler(scene):
     r.publish('test2', 'frame: %s' % i)
         
     euler_pose = PoseNeck.rotation_euler
-    r.publish('test', "Neck: z:%s, x:%s, y:%s" % (euler_pose[0], euler_pose[1], euler_pose[2]))
+    r.publish('test', "{'Neck': {'z':%s, 'x':%s, 'y':%s }}" % (euler_pose[0], euler_pose[1], euler_pose[2]))
     euler_pose = PoseHead.rotation_euler
-    r.publish('test', "Head: z:%s, x:%s, y:%s" % (euler_pose[0], euler_pose[1], euler_pose[2]))
+    r.publish('test', "{'Head': {'z':%s, 'x':%s, 'y':%s }}" % (euler_pose[0], euler_pose[1], euler_pose[2]))
+    euler_pose = PoseRElbow.rotation_euler
+    r.publish('test', "{'RElbow': {'z':%s, 'x':%s, 'y':%s }}" % (euler_pose[0], euler_pose[1], euler_pose[2]))
+    euler_pose = PoseLElbow.rotation_euler
+    r.publish('test', "{'LElbow': {'z':%s, 'x':%s, 'y':%s }}" % (euler_pose[0], euler_pose[1], euler_pose[2]))
+    euler_pose = PoseRShoulder.rotation_euler
+    r.publish('test', "{'RShoulder': {'z':%s, 'x':%s, 'y':%s }}" % (euler_pose[0], euler_pose[1], euler_pose[2]))
+    euler_pose = PoseLShoulder.rotation_euler
+    r.publish('test', "{'LShoulder': {'z':%s, 'x':%s, 'y':%s }}" % (euler_pose[0], euler_pose[1], euler_pose[2]))
     
 bpy.app.handlers.frame_change_pre.append(fhandler)
 print("Set handler")
